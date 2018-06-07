6c6afeff012110a9a11327f82fba84d1924d65d2
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-06-07 11:57:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}"
     holiday_hash.each do |holdiay, supplies|
       puts "#{holiday.to_s.split("_").map {|word| word.capitalize}.join(" ")} :
-      #{supplies.join(", ")}
+      #{supplies.join(", ")}"
 
 end
 

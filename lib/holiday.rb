252cf6d922007dd3b36b1559970cd2396c091978
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-06-07 12:37:56 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -15,14 +15,17 @@ def add_supply_to_memorial_day(holiday_hash, supply)
   holiday_supplies[:spring][:memorial_day] << supply
 end
 
+
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   holiday_hash[season][holiday_name] = supply_array
   holiday_hash
 end
 
+
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].values.flatten
 end
+
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
@@ -33,9 +36,8 @@ def all_supplies_in_holidays(holiday_hash)
   end
 end
 
+
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
   holiday_hash.collect do |season, holidays|
     holidays.collect do |holiday, supplies|
       holiday if supplies.include?("BBQ")
